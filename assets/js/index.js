d466168f19ba014f6ba0d5c255de5c1a74dc5fb6
Adriano <adrianosenny@gmail.com>
2023-02-24 09:11:26 -0300

    Finalizando projeto

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,45 @@
 const controle = document.querySelectorAll("[data-controll]");
+const estatisticas = document.querySelectorAll("[data-estatistica]");
+
+const physicals = {
+  altura: {
+    finalização: 29,
+    arremesso: 35,
+    jogada: -21,
+    defesa: 40,
+  },
+  peso: {
+    finalização: 41,
+    arremesso: 20,
+    jogada: 0,
+    defesa: -10,
+  },
+  velocidade: {
+    finalização: 0,
+    arremesso: 7,
+    jogada: 48,
+    defesa: -4,
+  },
+  força: {
+    finalização: 27,
+    arremesso: 0,
+    jogada: -11,
+    defesa: 40,
+  },
+  impulsao: {
+    finalização: 30,
+    arremesso: 10,
+    jogada: 0,
+    defesa: 10,
+  },
+};
 
 console.log(controle);
 
 controle.forEach((elemento) => {
   elemento.addEventListener("click", (evento) => {
     manipulaDados(evento.target.dataset.controll, evento.target.parentNode);
+    atualizaEstatisticas(evento.target.dataset.peca);
   });
 });
 
@@ -17,3 +52,13 @@ function manipulaDados(operacao, controle) {
     peca.value = parseInt(peca.value) + 1;
   }
 }
+
+function atualizaEstatisticas(peca) {
+  console.log(physicals[peca]);
+
+  estatisticas.forEach((elemento) => {
+    elemento.textContent = parseInt(elemento.textContent) + physicals[peca][elemento.dataset.estatistica]
+  })
+}
+
+function alterarJogador()
